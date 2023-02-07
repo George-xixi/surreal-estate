3dedf5104940f22e0a3ba047ce802d8caf4f77ac
George Aldington <georgealdington@gmail.com>
2023-02-07 12:30:17 +0000

    project setup and intial commit

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
+describe("App", () => {
+  it("renders app correctly", () => {
+    const { asFragment } = render(<App />);
+    const h1Element = screen.getByText("Surreal Estate");
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(h1Element).toBeInTheDocument();
+  })
+})
