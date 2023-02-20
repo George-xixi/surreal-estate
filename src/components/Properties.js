a379467fb90af7015250e5688a433eb6d01a9055
George Aldington <georgealdington@gmail.com>
2023-02-20 13:35:36 +0000

    renders all properties from API in properties component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,44 +1,33 @@
-import React from "react";
-import "../styles/properties.css";
+/* eslint-disable no-console */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Alert from "./Alert";
 import PropertyCard from "./PropertyCard";
+import "../styles/properties.css";
 
 const Properties = () => {
+  const [alert, setAlert] = useState({ message: "" });
+  const [properties, setProperties] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get("http://localhost:4000/api/v1/PropertyListing")
+        .then(({ data }) => setProperties(data));
+    } catch (e) {
+      setAlert({ message: "Server error. Please try again later." });
+      console.log(e);
+    }
+  }, []);
+
   return (
     <div className="properties">
       <h2 className="properties__heading">Properties</h2>
-      <PropertyCard
-        details={{
-          title: "title for property",
-          city: "Manchester",
-          type: "Flat",
-          bedrooms: 1,
-          bathrooms: 2,
-          price: 10000,
-          email: "test@test.com",
-        }}
-      />
-      <PropertyCard
-        details={{
-          title: "title for property",
-          city: "Manchester",
-          type: "Flat",
-          bedrooms: 1,
-          bathrooms: 2,
-          price: 10000,
-          email: "test@test.com",
-        }}
-      />
-      <PropertyCard
-        details={{
-          title: "title for property",
-          city: "Manchester",
-          type: "Flat",
-          bedrooms: 1,
-          bathrooms: 2,
-          price: 10000,
-          email: "test@test.com",
-        }}
-      />
+      <Alert message={alert.message} />
+      {properties.map((property) => (
+        <div key={property._id} className="card-div">
+          <PropertyCard key={property._id} details={property} />
+        </div>
+      ))}
     </div>
   );
 };
