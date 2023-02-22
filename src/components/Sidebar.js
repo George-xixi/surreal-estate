141d90172e75991b5511ccbaa6ac6997edb3498f
George Aldington <georgealdington@gmail.com>
2023-02-22 12:55:44 +0000

    creates sort by price capability

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,19 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/sidebar.css";
+
+const buildQueryString = (operation, valueObj) => {
+  const { search } = useLocation();
+
+  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+  const newQueryParams = {
+    ...currentQueryParams,
+    [operation]: JSON.stringify(valueObj),
+  };
+  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+};
 
 const Sidebar = () => {
   return (
@@ -10,18 +22,44 @@ const Sidebar = () => {
       <label htmlFor="collapsible" className="sidebar__header">
         Filter by :
       </label>
-      <div className="sidebar__div">
-        <Link className="sidebar__link" to={`/?query={"city": "Manchester"}`}>
+      <div className="sidebar__links">
+        <h3>City</h3>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("query", { city: "Manchester" })}
+        >
           Manchester
         </Link>
-        <Link className="sidebar__link" to={`/?query={"city": "Liverpool"}`}>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("query", { city: "Liverpool" })}
+        >
           Liverpool
         </Link>
-        <Link className="sidebar__link" to={`/?query={"city": "Leeds"}`}>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("query", { city: "Leeds" })}
+        >
           Leeds
         </Link>
-        <Link className="sidebar__link" to={`/?query={"city": "Sheffield"}`}>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("query", { city: "Sheffield" })}
+        >
           Sheffield
+        </Link>
+        <h3>Price</h3>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("sort", { price: 1 })}
+        >
+          high to low
+        </Link>
+        <Link
+          className="sidebar__link"
+          to={buildQueryString("sort", { price: -1 })}
+        >
+          low to high
         </Link>
       </div>
     </div>
